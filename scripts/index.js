5cb2fc537a1c898bb4a8eb6fe7787a34a3f685e4
Aleksandr Kim <112456407+KimAleksandr09041988@users.noreply.github.com>
2023-05-18 13:46:34 +0700

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,36 @@
+const form = document.querySelector('.form');
+const arrayInput = form.querySelectorAll('input');
+const listInputValue = {};
+let msg = listInputValue;
+
+const formInfo = () => {
+    arrayInput.forEach(item => {
+      if(item.type === 'text') {
+        return listInputValue[item.name] = item.value;
+      } else if(item.type === 'radio' && item.checked) {
+        return listInputValue[item.name] = item.value;
+      } else {
+        return ;
+      }
+    })
+};
+
+const telegram = {
+    "token": "6154114905:AAE5lxT4YveaJSGFB2C3BhLPpYuGiUs9ZJw",
+    "chat": "-873919155"
+}
+
+const server = () => {
+    fetch(`https://api.telegram.org/bot${telegram.token}/sendMessage?chat_id=${telegram.chat}&parse_mode=html&text=${JSON.stringify(msg).replace(/"/g, '')}`, {
+        method: 'POST',
+        'Content-Type': 'application/json',
+    }) 
+        .then(res => console.log(res))
+        .catch(req => console.log(req))
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    formInfo();
+    server();
+})
